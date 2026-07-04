2650fef68a48d952b7531c5ea3993111957fb738
seesoXdan <sokasky@gmail.com>
2026-07-05 08:48:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* DAN 일정 · 서비스워커 (오프라인 캐시) */
-const CACHE = 'dan-schedule-v2';
+const CACHE = 'dan-schedule-v4';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon-32.png', './favicon-16.png'];
 

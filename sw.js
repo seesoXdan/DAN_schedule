5552c21c62ffd40f655dd564866e7d8f49bbb35c
seesoXdan <sokasky@gmail.com>
2026-07-04 22:53:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* DAN 일정 · 서비스워커 (오프라인 캐시) */
+const CACHE = 'dan-schedule-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached ||
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
